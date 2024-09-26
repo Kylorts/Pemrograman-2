e000f95dee0414aa366fa769271add6b6f762ff3
Galih Aji Sabdaraya <144782996+Kylorts@users.noreply.github.com>
2024-09-26 21:12:32 +0800

    Update PRAK105_2310817210005_GalihAjiSabdaraya.java

--- Modul 1/PRAK105_2310817210005_GalihAjiSabdaraya.java
+++ Modul 1/PRAK105_2310817210005_GalihAjiSabdaraya.java
@@ -4,6 +4,7 @@ public class PRAK105_2310817210005_GalihAjiSabdaraya {
 
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
+		final double Pi = 3.14;
 		
 		System.out.print("Masukkan jari-jari: ");
 		double jari_jari = scan.nextDouble();
@@ -11,7 +12,7 @@ public class PRAK105_2310817210005_GalihAjiSabdaraya {
 		System.out.print("Masukkan tinggi: ");
 		double tinggi = scan.nextDouble();
 		
-		double volumeTabung = 3.14 * jari_jari * jari_jari * tinggi;
+		double volumeTabung = Pi * jari_jari * jari_jari * tinggi;
 		System.out.printf("Volume tabung dengan jari-jari %.1f dan tinggi %.1f cm adalah %.3f m3", jari_jari, tinggi, volumeTabung);
 		scan.close();
 	}
